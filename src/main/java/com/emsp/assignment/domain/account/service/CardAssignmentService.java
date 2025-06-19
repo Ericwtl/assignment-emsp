d594bae6bd3dcb8590a0c6044ec62d053474b2b8
Mino <25574528+Ericwtl@users.noreply.github.com>
2025-06-19 11:29:44 +0800

    fix a bug for card create.

--- src/main/java/com/emsp/assignment/domain/account/service/CardAssignmentService.java
+++ src/main/java/com/emsp/assignment/domain/account/service/CardAssignmentService.java
@@ -1,16 +1,8 @@
 package com.emsp.assignment.domain.account.service;
 
-import com.emsp.assignment.domain.account.model.Account;
-import com.emsp.assignment.domain.card.model.Card;
-import com.emsp.assignment.domain.card.model.CardStatus;
-import com.emsp.assignment.infrastructure.exception.AccountNotFoundException;
-import com.emsp.assignment.infrastructure.exception.CardNotFoundException;
 import com.emsp.assignment.infrastructure.persistence.AccountRepository;
 import com.emsp.assignment.infrastructure.persistence.CardRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 public class CardAssignmentService {
